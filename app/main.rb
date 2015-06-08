9b3a787912b0b6d5471989c0d23f82f97339f3b5
Oskar Calvo <oscar.calvo@the-cocktail.com>
2015-06-08 15:26:28 +0200

    agrego el directorio app y su contenido

---
+++ app/main.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+require 'net/http'
+require 'uri'
+require 'yaml'
+
+# Listen on all interfaces in the development environment
+set :bind, '0.0.0.0'
+
+get '/' do
+  "Just Do It"
+end
+
+get '/login' do
+  erb :loginform
+  
+end
+
+post '/loginvalidate' do
+	config = YAML.load_file("conf/config.yaml")
+	uri = URI.parse(config["config"]["url"], config["config"]["domain"])
+	req = Net::HTTP::Get.new(uri)
+	req.basic_auth config["config"]["user"], config["config"]["pass"]
+	res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+	  http.request(req)
+	}
+
+	"You said '#{params[:name]}' and '#{params[:password]}'"
+  
+end

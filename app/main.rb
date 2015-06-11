8ba74f9e184ad8bd053629e5748361aed588b959
Oskar Calvo <calvo.oskar@gmail.com>
2015-06-11 18:03:10 +0200

    Update main.rb
    probar nuevo código

--- app/main.rb
+++ app/main.rb
@@ -19,9 +19,10 @@ post '/loginvalidate' do
 	config = YAML.load_file("conf/config.yaml")
 	uri = URI.parse(config["config"]["url"], config["config"]["domain"])
 	req = Net::HTTP::Get.new(uri)
+	request = nil
 	req.basic_auth config["config"]["user"], config["config"]["pass"]
 	res = Net::HTTP.start(uri.hostname, uri.port) {|http|
-	  http.request(req)
+	  request = http.request(req)
 	}
 
 	"You said '#{params[:name]}' and '#{params[:password]}'"

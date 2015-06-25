f8d5dc61480b64173bb915d8f1d625aba9e02812
Oskar Calvo <oscar.calvo@the-cocktail.com>
2015-06-25 22:06:21 +0200

    actualización de código

---
+++ app/vendor/redmine_issues.rb
@@ -0,0 +1,14 @@
+def RedmineIssues
+
+  def getissues (path)
+
+    uri = URI.parse(path)
+    req = Net::HTTP::Get.new(uri)
+    response = nil
+    res = Net::HTTP.start(uri.hostname, uri.port) {|http|	  response = http.request(req)	}
+    binding.pry
+    return response  
+
+  end
+
+end

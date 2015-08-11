078142a0f31a7e6147bf3295957cd24e43ace525
Oskar Calvo <oscar.calvo@the-cocktail.com>
2015-08-11 17:21:27 +0200

    actualización de código para generar los selects en las vistas, el select de usuarios funciona correctamente, los otros dos selects no funcionan bien.

--- app/main.rb
+++ app/main.rb
@@ -1,8 +1,5 @@
-require 'rubygems'
+#require 'rubygems'
 require 'sinatra'
-require 'active_support'
-#require 'action_view'
-require 'sinatra_more' 
 require 'net/http'
 require 'uri'
 require 'yaml'
@@ -13,10 +10,38 @@ require 'sinatra/assetpack'
 
 require_relative 'vendor/redmine_user.rb'
 require_relative 'vendor/redmine_issues.rb'
+#require_relative 'helpers/redminehtmlhelper.rb'
 #http://recipes.sinatrarb.com/p/asset_management/sinatra_assetpack
 #http://blog.sourcing.io/structuring-sinatra
 
 # Listen on all interfaces in the development environment
+
+
+module Sinatra
+  module HtmlHelpers
+
+    # Basic implementation of a HTML SELECT helper
+    def select(options_list, selected_value)
+      html = ''
+      options_list.each do |nv_pair|
+        option_value = nv_pair[0]
+        option_name = nv_pair[1]
+        html << "<option value=\"#{option_value}\""
+        html << " selected=\"true\"" if option_value == selected_value
+        html << '>'
+        html << option_name
+        html << "</option>"
+      end
+      "<select >#{html}</select>"
+    end
+
+  end
+
+  helpers HtmlHelpers
+end
+
+
+
 
 configure do 
 set :bind, '0.0.0.0'
@@ -101,21 +126,27 @@ get '/project/:id' do
   
   pathmembers = @config['config']['url'] + 'projects/' + params[:id] + '/memberships.json'
   responsemembers = RedmineIssues.new.getprojectusers pathmembers,session
-  
-  miembros = nil
-
   if !responsemembers.nil?
-  
-    members = responsemembers['memberships'].inject({}) {|sum, elem| sum[elem['user']['id']] = elem['user']['name']; sum}
-    
-    miembros = members
+    @members = responsemembers['memberships'].inject({}) {|sum, elem| sum[elem['user']['id']] = elem['user']['name']; sum}
   end
-  
+
+  pathpriorities = @config['config']['url'] + 'enumerations/issue_priorities.json'
+  responsepriorities = RedmineIssues.new.getissuepriorities pathpriorities, session
+  if !responsepriorities.nil?
+    @priorities = responsepriorities['issue_priorities'].inject {|sum, elem| sum[elem['id']] = elem['name']; sum}
+  end
+
+  pathtracker = @config['config']['url'] + 'trackers.json'
+  responsetracker = RedmineIssues.new.getissuedata pathtracker, session
+  if !responsetracker.nil?
+    @trackers = responsetracker['trackers'].inject {|sum, elem| sum[elem['id']] = elem['name']; sum}
+  end
+
   #Pasamos la variable con la url de redmine.
-  #@path = @config['config']['url']
-  
-  #erb :issues
-  "Hello '#{miembros}' <br>  <br>  "
+  @path = @config['config']['url']
+
+  erb :issues
+  #{}"Hello '#{@members.inspect}' <br>  <br>  "
 
   
 end
--- app/vendor/redmine_issues.rb
+++ app/vendor/redmine_issues.rb
@@ -40,4 +40,46 @@ class RedmineIssues
   
   end
 
+
+  def getissuepriorities(path, session)
+
+    uri = URI.parse(path)
+    req = Net::HTTP::Get.new(uri)
+    response = nil
+    req.basic_auth session[:loginname], session[:loginpass]
+    res = Net::HTTP.start(uri.hostname, uri.port) {|http|   response = http.request(req)  }
+    #binding.pry
+    
+    
+    case response
+    when  Net::HTTPSuccess then
+      data = JSON.parse(response.body)
+      return data
+    else
+      return nil 
+    end   
+
+  end
+
+  def getissuedata(path, session)
+
+    uri = URI.parse(path)
+    req = Net::HTTP::Get.new(uri)
+    response = nil
+    req.basic_auth session[:loginname], session[:loginpass]
+    res = Net::HTTP.start(uri.hostname, uri.port) {|http|   response = http.request(req)  }
+    #binding.pry
+    
+    
+    case response
+    when  Net::HTTPSuccess then
+      data = JSON.parse(response.body)
+      return data
+    else
+      return nil 
+    end   
+
+  end
+
+
 end

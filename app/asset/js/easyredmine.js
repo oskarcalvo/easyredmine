ad4f502dd8c62214ab5827868223ee643a14b063
Oskar Calvo <oscar.calvo@the-cocktail.com>
2015-08-14 00:00:04 +0200

    primeros esbozos del código para recuperar los datos del formulario de incidencias

--- app/asset/js/easyredmine.js
+++ app/asset/js/easyredmine.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
 
-$(".saveissue").click(function () {
-  	alert("Handler for .click() called.");
-  }
-);
+	$(".saveissue").click(function () {
+  	
+		console.log($(this));
+  	//var issue = $(this).attr("#data-issue");
+  	var issue = $(this).attr('data-id');
+
+  	console.log( $(".prioriti_"+ issue).val());
+
+
+  	//alert("Handler for .click() called." + issue);
+
+  });
 
 });
